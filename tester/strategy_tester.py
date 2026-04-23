e51b915ef9b4d9f69c0a43f6152cca49ef030015
AB-code-crypto <alex-god@mail.ru>
2026-04-23 21:53:59 +0300

    исправил отправку графиков

--- tester/strategy_tester.py
+++ tester/strategy_tester.py
@@ -994,18 +994,18 @@ if __name__ == "__main__":
         "decision_min_final_move_points": [10.0],
 
         "similarity_weight_pearson": [4.0],
-        "similarity_weight_range": [1.0],
+        "similarity_weight_range": [0.0, 1.0, 2.0],
         "similarity_weight_net_move": [2.0],
         "similarity_weight_mean_abs_diff": [2.0],
         "similarity_weight_efficiency": [2.0],
         "similarity_weight_range_position": [2.0],
-        "similarity_weight_diff_pearson": [0.0],
-        "similarity_weight_diff_sign_match": [0.0],
+        "similarity_weight_diff_pearson": [0.0, 1.0, 2.0],
+        "similarity_weight_diff_sign_match": [0.0, 1.0, 2.0],
     }
     RESUME_FROM_EXISTING = False
 
     cpu_count = os.cpu_count() or 1
-    max_workers = min(16, cpu_count)
+    max_workers = min(18, cpu_count)
     chunk_size = 1
 
     price_db_path = settings_live.price_db_path
